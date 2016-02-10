1f9d67d181265a81d20b6cc4c47197d6458589f1
Quin Kennedy <quinkennedy@gmail.com>
2016-02-10 14:53:15 -0800

    updating project file for using shapefile

--- flatmaptrans/project.clj
+++ flatmaptrans/project.clj
@@ -3,6 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :repositories {"osgeo-geotools" "http://download.osgeo.org/webdav/geotools"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [quil "2.3.0"]
-                 [org.clojure/data.json "0.2.6"]])
+                 [org.clojure/data.json "0.2.6"]
+                 [org.geotools/gt-shapefile "2.7-M3"]])
